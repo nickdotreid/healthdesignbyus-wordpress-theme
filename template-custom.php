8ebd73d51876a782f796f42d5f06dbc03e8897f3
Nick Reid <nickreid@nickreid.com>
2014-06-08 12:38:37 -0400

    added page preview to pages template

--- template-custom.php
+++ template-custom.php
@@ -3,6 +3,20 @@
 Template Name: Custom Template
 */
 ?>
-
+<article class="wrap">
+	<div class="container">
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
+	</div>
+</article>
+<?
+$child_query = new WP_Query(array(
+	'post_type' => 'page',
+	'post_parent' => get_the_ID(),
+	));
+while($child_query->have_posts()){
+	$child_query->the_post();
+	include(locate_template('templates/preview.php'));
+}
+wp_reset_postdata();
+?>
---
+++ templates/preview.php
@@ -0,0 +1,15 @@
+<article <?php post_class(); ?>>
+	<div class="container">
+		<h2><a href="<?php the_permalink(); ?>"><? the_title(); ?></a></h2>
+		<div class="entry-summary">
+    		<?php the_excerpt(); ?>
+  		</div>
+  	<? if ( has_post_thumbnail() ): ?>
+  		<? $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large');	?>
+  		<a href="<?php the_permalink(); ?>">
+  			<img src="<?=$thumb[0];?>" alt="<? the_title(); ?>" />
+  		</a>
+  		<!-- SHOW POST TYPE IMAGE -->
+  	<? endif; ?>
+	</div>
+</article>
