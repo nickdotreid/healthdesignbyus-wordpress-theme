225e32f99762f017b1431c928d341d80fc0cc31b
Nick Reid <nickreid@nickreid.com>
2014-06-08 15:05:40 -0400

    tweaked mobile styles and added sprite sheet

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,6 +1,5 @@
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
-    <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
@@ -8,14 +7,11 @@
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
-    </div>
-    <nav class="">
       <?php
         if (has_nav_menu('share_links')) :
-          wp_nav_menu(array('theme_location' => 'share_links', 'menu_class' => 'nav navbar-nav navbar-right navbar-icons'));
+          wp_nav_menu(array('theme_location' => 'share_links', 'menu_class' => 'nav navbar-nav navbar-right navbar-icons pull-right'));
         endif;
-      ?>   
-    </nav>
+      ?>
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
