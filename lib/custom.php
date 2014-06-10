ca0378f8ed14047fbc8eb531f050acf0ca260dfc
Nick Reid <nickreid@nickreid.com>
2014-06-10 17:41:47 -0400

    made nav icons back into squares, not rectangles

--- lib/custom.php
+++ lib/custom.php
@@ -7,3 +7,42 @@ function register_menu() {
 	 register_nav_menu( 'share_links', _('Main share links.') );
 }
 add_action('init', 'register_menu');
+
+function hdbus_register_custom_fields(){
+	if(function_exists("register_field_group"))
+	{
+		register_field_group(array (
+			'id' => 'acf_post-color',
+			'title' => 'Post Color',
+			'fields' => array (
+				array (
+					'key' => 'field_5393808fedeb8',
+					'label' => 'Color',
+					'name' => 'color',
+					'type' => 'color_picker',
+					'instructions' => 'Pick a color to use as the background color',
+					'default_value' => '',
+				),
+			),
+			'location' => array (
+				array (
+					array (
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'page',
+						'order_no' => 0,
+						'group_no' => 0,
+					),
+				),
+			),
+			'options' => array (
+				'position' => 'normal',
+				'layout' => 'no_box',
+				'hide_on_screen' => array (
+				),
+			),
+			'menu_order' => 0,
+		));
+	}
+}
+add_action('init', 'hdbus_register_custom_fields');
--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '23404be915dd3a646105b2baa26c03e8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f9ee691d36f8f7affafb373ab263838d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
